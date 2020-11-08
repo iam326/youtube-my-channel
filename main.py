9c746e7f4b0819b1c9c271cd16c90ea027949753
t.takahashi <takahashi.developer@gmail.com>
2020-11-08 23:09:43 +0900

    get my channel and video info

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from youtube_data_api_client import YoutubeDataApiClient
 
 YOUTUBE_DATA_CLIENT_SECRETS_FILE = "client_secrets.json"
 YOUTUBE_DATA_API_CLIENT_SCOPES = [
-    'https://www.googleapis.com/auth/youtube.force-ssl']
+    'https://www.googleapis.com/auth/youtube.readonly']
 
 
 def main():
     youtube = YoutubeDataApiClient(
         YOUTUBE_DATA_CLIENT_SECRETS_FILE, YOUTUBE_DATA_API_CLIENT_SCOPES)
-    print(youtube)
+
+    channel = youtube.get_my_channel()
+    videos = youtube.get_my_videos()
+    print(videos)
 
 
 if __name__ == '__main__':
--- youtube_data_api_client.py
+++ youtube_data_api_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import httplib2
 import os
+
+import httplib2
 
 from apiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
@@ -36,7 +37,7 @@ class YoutubeDataApiClient():
                                        scope=scopes,
                                        message=message)
 
-        storage = Storage("youtube-oauth2.json")
+        storage = Storage('youtube-oauth2.json')
         credentials = storage.get()
 
         if credentials is None or credentials.invalid:
@@ -44,3 +45,61 @@ class YoutubeDataApiClient():
 
         return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                      http=credentials.authorize(httplib2.Http()))
+
+    def get_my_channel(self):
+        channels = self.__client.channels().list(
+            part='snippet,statistics', mine=True).execute()
+        channel = channels['items'][0]
+        snippet = channel['snippet']
+        statistics = channel['statistics']
+        return {
+            'id': channel['id'],
+            'title': snippet['title'],
+            'description': snippet['description'],
+            'video_count': statistics['videoCount'],
+            'view_count': statistics['viewCount'],
+            'subscriber_count': statistics['subscriberCount'],
+        }
+
+    def get_my_videos(self):
+        search_list_request = self.__client.search().list(
+            part='id', forMine=True, type='video', order='date', maxResults=50)
+
+        video_ids = []
+        while search_list_request:
+            search_list_response = search_list_request.execute()
+
+            for video in search_list_response['items']:
+                video_ids.append(video['id']['videoId'])
+
+            search_list_request = self.__client.search().list_next(
+                search_list_request, search_list_response)
+
+        videos = []
+        for ids in self.__chunks(video_ids, 50):
+            videos_list = self.__client.videos().list(
+                id=','.join(ids),
+                part='snippet,contentDetails,statistics',
+                fields='items(id,snippet(title,description,publishedAt),contentDetails(duration),statistics(viewCount,likeCount,dislikeCount,commentCount))'
+            ).execute()
+            for item in videos_list['items']:
+                snippet = item['snippet']
+                details = item['contentDetails']
+                statistics = item['statistics']
+                videos.append({
+                    'id': item['id'],
+                    'title': snippet['title'],
+                    'description': snippet['description'],
+                    'published_at': snippet['publishedAt'],
+                    'duration': details['duration'],
+                    'view_count': statistics['viewCount'],
+                    'like_count': statistics['likeCount'],
+                    'dislike_count': statistics['dislikeCount'],
+                    'comment_count': statistics['commentCount']
+                })
+
+        return videos
+
+    def __chunks(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
