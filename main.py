a12367484cb67a2ac187a335eb0f48bba3209163
t.takahashi <takahashi.developer@gmail.com>
2020-11-27 22:51:56 +0900

    output result

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 from youtube_data_api_client import YoutubeDataApiClient
 
 YOUTUBE_DATA_CLIENT_SECRETS_FILE = "client_secrets.json"
@@ -14,11 +16,25 @@ def main():
     channel = youtube.get_my_channel()
     videos = youtube.get_my_videos()
 
-    total_seconds = 0
+    total = {
+        'duration': 0,
+        'like_count': 0,
+        'dislike_count': 0
+    }
     for video in videos:
-        total_seconds += video['duration']
+        total['duration'] += video['duration']
+        total['like_count'] += video['like_count']
+        total['dislike_count'] += video['dislike_count']
 
-    print(total_seconds / 60 / 60)
+    total_hours = total['duration'] / 60 / 60
+
+    print(channel['title'])
+    print('配信回数:', channel['video_count'])
+    print('総視聴回数:', channel['view_count'])
+    print('チャンネル登録者数', channel['subscriber_count'])
+    print('総高評価数', total['like_count'])
+    print('総低評価数', total['dislike_count'])
+    print('総配信時間', math.floor(total_hours))
 
 
 if __name__ == '__main__':
